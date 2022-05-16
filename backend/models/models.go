dfadd5ef33fd9a471a1ea302ebcf7912f2d4703f
p267sing <p267sing@uwaterloo.ca>
2022-05-16 12:22:40 -0400

    finished crud-api and added redis to docker-compose.yml

--- backend/models/models.go
+++ backend/models/models.go
@@ -6,14 +6,27 @@ import (
 
 type User struct {
 	gorm.Model
-	Id       uint   `gorm:"primaryKey;autoIncrement:true"`
 	Username string `gorm:"unique"`
+	Pfp      string
 }
 type Task struct {
 	gorm.Model
-	Id          uint `gorm:"primaryKey;autoIncrement:true"`
-	Assignee    uint `gorm:"foreignKey:users_Id"`
+	Assignee    uint `gorm:"foreignKey:users_Username"`
+	Creator     uint `gorm:"foreignKey:users_Username"`
 	Description string
 	Name        string
 	Status      string
+}
+
+type Search struct {
+	//Assignee []string
+	//NotAssignee bool //apply NOT to users
+	/*Time1        time.Time
+	Time2        time.Time
+	TimeInterval string //one of 'before', 'after', 'in interval', 'out of interval'*/
+	//Status      string
+	ID string `json:"id"`
+	/*DeletedAt   gorm.DeletedAt `gorm:"index"`
+	Description string
+	Name        string*/
 }
